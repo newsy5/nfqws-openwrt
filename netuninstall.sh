bf0f0a72889d8fef8daad44dd42750fd1149e0a8
newsy5 <vladimir@ohelp.ru>
2024-09-02 10:32:45 +0300

    Update netuninstall.sh

--- netuninstall.sh
+++ netuninstall.sh
@@ -2,18 +2,18 @@
 
 ABSOLUTE_FILENAME=`readlink -f "$0"`
 HOME_FOLDER=`dirname "$ABSOLUTE_FILENAME"`
-BASE_URL="https://raw.githubusercontent.com/Anonym-tsk/nfqws-keenetic/master"
+BASE_URL="https://raw.githubusercontent.com/newsy5/nfqws-openwrt/master"
 
 cd /tmp
-mkdir -p nfqws-keenetic/common
+mkdir -p nfqws-openwrt/common
 
-curl -SL# "$BASE_URL/uninstall.sh" -o "nfqws-keenetic/uninstall.sh"
-curl -SL# "$BASE_URL/common/install_func.sh" -o "nfqws-keenetic/common/install_func.sh"
+curl -SL# "$BASE_URL/uninstall.sh" -o "nfqws-openwrt/uninstall.sh"
+curl -SL# "$BASE_URL/common/install_func.sh" -o "nfqws-openwrt/common/install_func.sh"
 
-chmod +x ./nfqws-keenetic/*.sh
-./nfqws-keenetic/uninstall.sh
+chmod +x ./nfqws-openwrt/*.sh
+./nfqws-openwrt/uninstall.sh
 
-rm -rf nfqws-keenetic
+rm -rf nfqws-openwrt
 cd $HOME_FOLDER
 
 exit 0
