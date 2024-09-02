43bd12546734c43c3b6d1f407275670eb68e9c8e
newsy5 <vladimir@ohelp.ru>
2024-09-02 10:31:43 +0300

    Update netinstall.sh

--- netinstall.sh
+++ netinstall.sh
@@ -2,27 +2,27 @@
 
 ABSOLUTE_FILENAME=`readlink -f "$0"`
 HOME_FOLDER=`dirname "$ABSOLUTE_FILENAME"`
-BASE_URL="https://raw.githubusercontent.com/Anonym-tsk/nfqws-keenetic/master"
+BASE_URL="https://raw.githubusercontent.com/newsy5/nfqws-openwrt/master"
 
 cd /tmp
-mkdir -p nfqws-keenetic/etc/nfqws
-mkdir -p nfqws-keenetic/etc/init.d
-mkdir -p nfqws-keenetic/etc/ndm/netfilter.d
-mkdir -p nfqws-keenetic/common
+mkdir -p nfqws-openwrt/etc/nfqws
+mkdir -p nfqws-openwrt/etc/init.d
+mkdir -p nfqws-openwrt/etc/ndm/netfilter.d
+mkdir -p nfqws-openwrt/common
 
-curl -SL# "$BASE_URL/install.sh" -o "nfqws-keenetic/install.sh"
-curl -SL# "$BASE_URL/common/install_func.sh" -o "nfqws-keenetic/common/install_func.sh"
-curl -SL# "$BASE_URL/etc/nfqws/nfqws.conf" -o "nfqws-keenetic/etc/nfqws/nfqws.conf"
-curl -SL# "$BASE_URL/etc/nfqws/user.list" -o "nfqws-keenetic/etc/nfqws/user.list"
-curl -SL# "$BASE_URL/etc/nfqws/auto.list" -o "nfqws-keenetic/etc/nfqws/auto.list"
-curl -SL# "$BASE_URL/etc/nfqws/exclude.list" -o "nfqws-keenetic/etc/nfqws/exclude.list"
-curl -SL# "$BASE_URL/etc/init.d/S51nfqws" -o "nfqws-keenetic/etc/init.d/S51nfqws"
-curl -SL# "$BASE_URL/etc/ndm/netfilter.d/100-nfqws.sh" -o "nfqws-keenetic/etc/ndm/netfilter.d/100-nfqws.sh"
+curl -SL# "$BASE_URL/install.sh" -o "nfqws-openwrt/install.sh"
+curl -SL# "$BASE_URL/common/install_func.sh" -o "nfqws-openwrt/common/install_func.sh"
+curl -SL# "$BASE_URL/etc/nfqws/nfqws.conf" -o "nfqws-openwrtc/etc/nfqws/nfqws.conf"
+curl -SL# "$BASE_URL/etc/nfqws/user.list" -o "nfqws-openwrt/etc/nfqws/user.list"
+curl -SL# "$BASE_URL/etc/nfqws/auto.list" -o "nfqws-openwrt/etc/nfqws/auto.list"
+curl -SL# "$BASE_URL/etc/nfqws/exclude.list" -o "nfqws-openwrt/etc/nfqws/exclude.list"
+curl -SL# "$BASE_URL/etc/init.d/S51nfqws" -o "nfqws-openwrt/etc/init.d/S51nfqws"
+#curl -SL# "$BASE_URL/etc/ndm/netfilter.d/100-nfqws.sh" -o "nfqws-openwrt/etc/ndm/netfilter.d/100-nfqws.sh"
 
-chmod +x ./nfqws-keenetic/*.sh
-./nfqws-keenetic/install.sh
+chmod +x ./nfqws-openwrt/*.sh
+./nfqws-openwrt/install.sh
 
-rm -rf nfqws-keenetic
+rm -rf nfqws-openwrt
 cd $HOME_FOLDER
 
 exit 0
